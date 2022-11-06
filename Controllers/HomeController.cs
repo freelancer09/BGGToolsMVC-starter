993d17271b107042ba6c6316459919c52a78b9ed
freelancer09 <2009.mike@gmail.com>
2022-11-05 21:04:40 -0400

    Initial blank state

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,8 +16,6 @@ namespace BGGToolsMvC.Controllers
     {
         private IWebHostEnvironment Environment;
 
-        const string BGG_API_URL = "https://api.geekdo.com/xmlapi2/";
-
         public HomeController(IWebHostEnvironment _environment)
         {
             this.Environment = _environment;
@@ -29,131 +27,19 @@ namespace BGGToolsMvC.Controllers
         }
 
         [HttpGet]
-        public IActionResult CollectionView(string userName = null)
+        public IActionResult CollectionView()
         {
-            if (userName == null)
-            {
-                return View();
-            }
-            else
-            {
-                List<Thing> things = new List<Thing>();
-
-                // Load the API XML document
-                XmlDocument doc = new XmlDocument();
-                doc.Load(string.Concat(BGG_API_URL, "collection?username=", userName));
-
-                // If a collection is loaded, process the THINGS
-                if (doc.DocumentElement.Name == "items")
-                {
-                    // Loop through the selected Nodes using XPATH
-                    foreach (XmlNode node in doc.SelectNodes("/items/item"))
-                    {
-                        string year = "N/A";
-                        if (node["yearpublished"] != null)
-                            year = node["yearpublished"].InnerText;
-
-                        // Fetch the Node values and assign it to Model
-                        things.Add(new Thing
-                        {
-                            ThingId = int.Parse(node.Attributes["objectid"].InnerText),
-                            Name = node["name"].InnerText,
-                            YearPublished = year
-                        });
-                    }
-                    ViewBag.UserName = userName;
-                }
-
-                // Check for errors and return the message
-                else if (doc.DocumentElement.Name == "errors")
-                {
-                    string errorMessage = null;
-                    foreach (XmlNode node in doc.SelectNodes("/errors/error"))
-                    {
-                        errorMessage += node["message"].InnerText + " ";
-                    }
-                    ViewBag.ErrorMessage = errorMessage;
-                }
-
-                // Check for errors and return the message
-                else if (doc.DocumentElement.Name == "message")
-                {
-                    ViewBag.ErrorMessage = doc.DocumentElement.InnerText;
-                }
-
-                // Send list of THING to the view
-                return View(things);
-            }
+            return View();
         }
 
         public IActionResult HotList()
         {
-            List<Thing> things = new List<Thing>();
-
-            // Load the API XML document
-            XmlDocument doc = new XmlDocument();
-            doc.Load(string.Concat(BGG_API_URL, "hot"));
-
-            // Loop through the selected Nodes using XPATH
-            foreach (XmlNode node in doc.SelectNodes("/items/item"))
-            {
-                string year = "N/A";
-                if (node["yearpublished"] != null)
-                    year = node["yearpublished"].GetAttribute("value");
-                
-                // Fetch the Node values and assign it to Model
-                things.Add(new Thing
-                {
-                    ThingId = int.Parse(node.Attributes["id"].InnerText),
-                    Name = node["name"].GetAttribute("value"),
-                    YearPublished = year
-                });
-            }
-
-            // Send List of THING to the view
-            return View(things);
+            return View();
         }
 
-        [Route("Home/Thing/{id}")]
-        public IActionResult Thing(int id)
+        public IActionResult Thing()
         {
-            Thing thing = new Thing();
-
-            // Load the API XML document
-            XmlDocument doc = new XmlDocument();
-            doc.Load(string.Concat(BGG_API_URL, "thing?id=", id));
-
-            // Loop through the selected Nodes using XPATH and assign to Model
-            foreach (XmlNode node in doc.SelectNodes("items/item"))
-            {
-                thing.ThingId = int.Parse(node.Attributes["id"].InnerText);
-
-                thing.Name = node["name"].GetAttribute("value");
-
-                thing.Description = node["description"].InnerText;
-
-                string year = "N/A";
-                if (node["yearpublished"] != null)
-                    year = node["yearpublished"].GetAttribute("value");
-                thing.YearPublished = year;
-
-                thing.Thumbnail = node["thumbnail"].InnerText;
-
-                thing.Image = node["image"].InnerText;
-
-                thing.MinPlayers = int.Parse(node["minplayers"].GetAttribute("value"));
-
-                thing.MaxPlayers = int.Parse(node["maxplayers"].GetAttribute("value"));
-
-                thing.MinPlayTime = int.Parse(node["minplaytime"].GetAttribute("value"));
-
-                thing.MaxPlayTime = int.Parse(node["maxplaytime"].GetAttribute("value"));
-
-                thing.MinAge = int.Parse(node["minage"].GetAttribute("value"));
-            }
-
-            // Send THING to the view
-            return View(thing);
+            return View();
         }
 
         public IActionResult Privacy()
